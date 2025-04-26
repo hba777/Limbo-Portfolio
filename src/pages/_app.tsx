4775e6c1823ee2bbb5426fcc44feca12258ee27e
Haris Bin Amir <hba777@outlook.com>
2025-04-26 23:55:27 +0500

    Music Player Removed Temporarily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-      <MusicPlayer />
+      {/* <MusicPlayer /> */}
     </main>
   )
 }
